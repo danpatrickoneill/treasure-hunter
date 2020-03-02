688550771b09dbd5325abbe92857cb7348da50ce
danpatrickoneill <dan47573@gmail.com>
2020-03-02 12:14:06 -0500

    refactored to use key from .env file

--- treasure-hunter/src/utils/axiosWithAuth.js
+++ treasure-hunter/src/utils/axiosWithAuth.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+token = REACT_APP_API_KEY
 
-export const axiosWithAuth = token => {
+export const axiosWithAuth = () => {
   return axios.create({
     headers: {
       Authorization: `Token ${token}`
